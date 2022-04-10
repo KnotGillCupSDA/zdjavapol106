f89783d611d07a50a296230b64566a1fbe5f8be7
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 13:41:02 +0200

    -- zad7 impl draft

---
+++ src/main/java/com/sda/advanced/solution/zad7/MyJacket.java
@@ -0,0 +1,24 @@
+package com.sda.advanced.solution.zad7;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MyJacket implements Jacket {
+
+	private final Deque<String> stack = new ArrayDeque<>();
+
+	@Override
+	public void loadBullet(String bullet) {
+
+	}
+
+	@Override
+	public boolean isLoaded() {
+		return false;
+	}
+
+	@Override
+	public String shot() {
+		return null;
+	}
+}
