c0dafd35f2e750925645d7e3fa914d4e3fb399a0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 10:20:22 +0200

    -- zad3 draft

---
+++ src/main/java/com/sda/advanced/solution/zad3/Main.java
@@ -0,0 +1,24 @@
+package com.sda.advanced.solution.zad3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Java", 18);
+		map.put("C#", 9);
+		map.put("Python", 1);
+		map.put("JavaScript", 0);
+		map.put("PHP", 0);
+
+		print(map);
+
+	}
+
+	private static void print(Map<String, Integer> map) {
+
+	}
+
+}
