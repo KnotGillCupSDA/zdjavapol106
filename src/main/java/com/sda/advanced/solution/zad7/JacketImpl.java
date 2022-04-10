0e4572af265f4cd21470ddac19c3518404500868
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 14:33:00 +0200

    -- zad7 midwork

--- src/main/java/com/sda/advanced/solution/zad7/JacketImpl.java
+++ src/main/java/com/sda/advanced/solution/zad7/JacketImpl.java
@@ -6,17 +6,21 @@ import java.util.Deque;
 public class JacketImpl implements Jacket {
 
 	private final Deque<String> stack = new ArrayDeque<>();
+	private final int maxSize;
 
 	public JacketImpl() {
 		this(10);
 	}
 
 	public JacketImpl(int maxSize) {
-
+		this.maxSize = maxSize;
 	}
 
 	@Override
 	public void loadBullet(String bullet) {
+		//if is full then throw exception
+
+		//else add to deque/stack
 		stack.add(bullet);
 	}
 
