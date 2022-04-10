dfac23d0de170cd3f2e21d87de79baf3ff452c1f
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 11:47:12 +0200

    -- zad4 scratch + printValues

---
+++ src/main/java/com/sda/advanced/solution/zad4/Storage.java
@@ -0,0 +1,15 @@
+package com.sda.advanced.solution.zad4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+	private final Map<String, List<String>> map = new HashMap<>();
+
+	public void printValues(String key) {
+		List<String> strings = map.get(key);
+		System.out.println(strings);
+	}
+
+}
