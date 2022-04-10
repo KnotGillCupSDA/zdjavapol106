751c892ef2d0483f8fb0283ce4b9da67a61f69cc
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 09:27:51 +0200

    -- zad1 draft

---
+++ src/main/java/com/sda/advanced/solution/zad1/Main.java
@@ -0,0 +1,21 @@
+package com.sda.advanced.solution.zad1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> unsorted = new ArrayList<>();
+		System.out.println(unsorted);
+
+		List<String> sorted = sort(unsorted);
+
+		System.out.println("===");
+		System.out.println(sorted);
+	}
+
+	private static List<String> sort(List<String> unsorted) {
+		return null;
+	}
+}
