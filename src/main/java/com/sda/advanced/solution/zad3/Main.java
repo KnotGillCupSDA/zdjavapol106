d003e1e18437c4467d6e7a3327d038606599ae24
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-10 11:02:09 +0200

    -- zad3 done

--- src/main/java/com/sda/advanced/solution/zad3/Main.java
+++ src/main/java/com/sda/advanced/solution/zad3/Main.java
@@ -1,19 +1,31 @@
 package com.sda.advanced.solution.zad3;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Map<String, Integer> map = new HashMap<>();
+		/*Map<String, Integer> map = new HashMap<>();
 		map.put("Java", 18);
 		map.put("C#", 9);
 		map.put("Python", 1);
 		map.put("JavaScript", 0);
-		map.put("PHP", 0);
+		map.put("PHP", 0);*/
+
+		//java 11 style - immutable map
+		Map<String, Integer> map = Map.of("Java", 18,
+				"C#", 9,
+				"Python", 1,
+				"JavaScript", 0,
+				"PHP", 0);
 
 		print(map);
+		System.out.println("===");
+		printWithForeach(map);
+		System.out.println("===");
+		printWithStream(map);
 
 	}
 
@@ -27,12 +39,31 @@ public class Main {
 		for (Map.Entry<String, Integer> entry : map.entrySet()) {
 			boolean isLastElement = index++ == map.size() - 1;
 			if (isLastElement) {
-				System.out.println("Klucz: " + entry.getKey() + " " + "Wartość: " + entry.getValue() + ".");
+				System.out.println("Klucz: " + entry.getKey() + ", " + "Wartość: " + entry.getValue() + ".");
 			} else {
-				System.out.println("Klucz: " + entry.getKey() + " " + "Wartość: " + entry.getValue() + ",");
+				System.out.println("Klucz: " + entry.getKey() + ", " + "Wartość: " + entry.getValue() + ",");
 			}
 		}
 
+	}
+
+	private static void printWithForeach(Map<String, Integer> map) {
+		AtomicInteger index = new AtomicInteger(0);
+		map.forEach((key, value) -> {
+			boolean isLastElement = index.getAndIncrement() == map.size() - 1;
+			if (isLastElement) {
+				System.out.println("Klucz: " + key + ", " + "Wartość: " + value + ".");
+			} else {
+				System.out.println("Klucz: " + key + ", " + "Wartość: " + value + ",");
+			}
+		});
+	}
+
+	private static void printWithStream(Map<String, Integer> map) {
+		String collect = map.entrySet().stream()
+				.map(entry -> "Klucz: " + entry.getKey() + ", " + "Wartość: " + entry.getValue())
+				.collect(Collectors.joining("," + System.lineSeparator(), "", "."));
+		System.out.println(collect);
 	}
 
 }
