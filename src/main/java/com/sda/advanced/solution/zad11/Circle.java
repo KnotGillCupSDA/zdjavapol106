a11b6998e986fcaf674401adf2cb147f202561c1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-23 11:00:06 +0200

    -- Zad 11 scratch

---
+++ src/main/java/com/sda/advanced/solution/zad11/Circle.java
@@ -0,0 +1,44 @@
+package com.sda.advanced.solution.zad11;
+
+public class Circle implements Movable {
+
+	private final Point2D center;
+	private final double radius;
+
+	public Circle(Point2D center, Point2D point) {
+		this.center = center;
+		this.radius = calculateRadius(point);
+	}
+
+	private double calculateRadius(Point2D point) {
+		System.out.println("Calculating radius");
+		double poweredDifferenceBetweenTwoXCoordinates = Math.pow((center.getX()
+				- point.getX()), 2);
+		double poweredDifferenceBetweenTwoYCoordinates = Math.pow((center.getY()
+				- point.getY()), 2);
+
+		return Math.sqrt(poweredDifferenceBetweenTwoXCoordinates + poweredDifferenceBetweenTwoYCoordinates);
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public double getPerimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+	public double getArea() {
+		//return Math.PI * getRadius() * getRadius();
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+	@Override
+	public void move(MoveDirection moveDirection) {
+		//move center
+		center.move(moveDirection);
+		//center.setX(center.getX() + moveDirection.getX());
+		//center.setY(center.getY() + moveDirection.getY());
+	}
+
+}
