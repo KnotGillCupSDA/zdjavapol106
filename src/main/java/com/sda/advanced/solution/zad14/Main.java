d034752a3fe913182eaada64e0d82b682fd531a4
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-23 15:32:36 +0200

    -- Zad 14 filling array with random integers

--- src/main/java/com/sda/advanced/solution/zad14/Main.java
+++ src/main/java/com/sda/advanced/solution/zad14/Main.java
@@ -1,19 +1,28 @@
 package com.sda.advanced.solution.zad14;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
+
+	public static final int BOUND = 1001;
 
 	public static void main(String[] args) {
 		int[] array = new int[100_000];
 		fill(array);
+
+		System.out.println(Arrays.toString(array));
 
 		List<Integer> distinct = distinct(array);
 
 	}
 
 	private static void fill(int[] array) {
-
+		Random random = new Random();
+		for (int i = 0; i < 100000; i++) {
+			array[i] = random.nextInt(BOUND);
+		}
 	}
 
 	private static List<Integer> distinct(int[] array) {
