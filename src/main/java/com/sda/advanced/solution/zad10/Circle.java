0791c47fe91db71959853eb727620ff29a7f3abe
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-23 10:03:58 +0200

    -- Zad 10 scratch

---
+++ src/main/java/com/sda/advanced/solution/zad10/Circle.java
@@ -0,0 +1,39 @@
+package com.sda.advanced.solution.zad10;
+
+public class Circle {
+
+	private final Point2D center;
+	private final Point2D point;
+
+	private final double radius;
+
+	public Circle(Point2D center, Point2D point) {
+		this.center = center;
+		this.point = point;
+		this.radius = calculateRadius();
+	}
+
+	private double calculateRadius() {
+		System.out.println("Calculating radius");
+		double poweredDifferenceBetweenTwoXCoordinates = Math.pow((center.getX()
+				- point.getX()), 2);
+		double poweredDifferenceBetweenTwoYCoordinates = Math.pow((center.getY()
+				- point.getY()), 2);
+
+		return Math.sqrt(poweredDifferenceBetweenTwoXCoordinates + poweredDifferenceBetweenTwoYCoordinates);
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public double getPerimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+	public double getArea() {
+		//return Math.PI * getRadius() * getRadius();
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+}
