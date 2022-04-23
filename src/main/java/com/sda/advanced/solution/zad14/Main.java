c2badc0a273650a647edd9fd32e9479d1f318b8c
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-23 15:28:13 +0200

    -- Zad 14 scratch

---
+++ src/main/java/com/sda/advanced/solution/zad14/Main.java
@@ -0,0 +1,23 @@
+package com.sda.advanced.solution.zad14;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] array = new int[100_000];
+		fill(array);
+
+		List<Integer> distinct = distinct(array);
+
+	}
+
+	private static void fill(int[] array) {
+
+	}
+
+	private static List<Integer> distinct(int[] array) {
+		return null;
+	}
+
+}
