66e5b8c5fd5414823fb70fb585aafb3cd08f6e02
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-23 12:02:25 +0200

    -- Zad 12 done+

--- src/main/java/com/sda/advanced/solution/zad12/Main.java
+++ src/main/java/com/sda/advanced/solution/zad12/Main.java
@@ -6,6 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Manufacturer fiat = new Manufacturer("Fiat", 1900, "Italy");
+		Manufacturer fiat2 = fiat;
+		Manufacturer fiat3 = new Manufacturer("Fiat", 1900, "Italy");
+
+		//compare the same reference
+		System.out.println(fiat == fiat2);
+		System.out.println(fiat.equals(fiat2));
+
+		//compare different instance having same field values
+		System.out.println(fiat == fiat3);
+		System.out.println(fiat.equals(fiat3));
 
 		Car panda = new Car("Panda",
 				"Fiat",
