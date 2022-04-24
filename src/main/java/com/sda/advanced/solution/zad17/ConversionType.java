b99a5cf443e8862759aac882d2459c608baab1ef
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-24 10:30:29 +0200

    -- Zad 17 with factor

--- src/main/java/com/sda/advanced/solution/zad17/ConversionType.java
+++ src/main/java/com/sda/advanced/solution/zad17/ConversionType.java
@@ -1,10 +1,21 @@
 package com.sda.advanced.solution.zad17;
 
 public enum ConversionType {
-	METERS_TO_YARDS,
-	YARDS_TO_METERS,
-	CENTIMETERS_TO_INCHES,
-	INCHES_TO_CENTIMETERS,
-	KILOMETERS_TO_MILES,
-	MILES_TO_KILOMETERS
+
+	METERS_TO_YARDS(1.09361),
+	YARDS_TO_METERS(0.9144),
+	CENTIMETERS_TO_INCHES(0.393701),
+	INCHES_TO_CENTIMETERS(2.54),
+	KILOMETERS_TO_MILES(0.621371),
+	MILES_TO_KILOMETERS(1.60934);
+
+	private final double factor;
+
+	ConversionType(double factor) {
+		this.factor = factor;
+	}
+
+	public double getFactor() {
+		return factor;
+	}
 }
--- src/main/java/com/sda/advanced/solution/zad17/MeasurementConverter.java
+++ src/main/java/com/sda/advanced/solution/zad17/MeasurementConverter.java
@@ -2,7 +2,7 @@ package com.sda.advanced.solution.zad17;
 
 public class MeasurementConverter {
 	public double convert(double value, ConversionType conversionType) {
-		return 0.0;
+		return value * conversionType.getFactor();
 	}
 
 }
