cf3eff17e69e85980f770327d000bb436cde814a
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-24 12:52:29 +0200

    -- zad 30 read file

--- src/main/java/com/sda/advanced/solution/zad30/Main.java
+++ src/main/java/com/sda/advanced/solution/zad30/Main.java
@@ -1,14 +1,19 @@
 package com.sda.advanced.solution.zad30;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Path workingDirectory = Paths.get("src", "main", "resources", "zad30");
-		System.out.println(workingDirectory.resolve("someTextFile.txt").toFile().exists());
+		System.out.println("exists? " + workingDirectory.resolve("someTextFile.txt").toFile().exists());
 
+		Path pathToFile = workingDirectory.resolve("someTextFile.txt");
+		String textFromFile = Files.readString(pathToFile);
+		System.out.println(textFromFile);
 	}
 }
