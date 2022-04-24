d2afb44e8a0651f00e21d953f874afc50762c2cc
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-04-24 12:03:34 +0200

    -- Zad 27 done

--- src/main/java/com/sda/advanced/solution/zad27/Joiner.java
+++ src/main/java/com/sda/advanced/solution/zad27/Joiner.java
@@ -1,11 +1,33 @@
 package com.sda.advanced.solution.zad27;
 
-import java.util.List;
-
 public class Joiner<T> {
 
-	public String join(String delimiter, T... parts) {
-		return null;
+	private final String separator;
+
+	public Joiner(String separator) {
+		this.separator = separator;
+	}
+
+	public String join(T... elements) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < elements.length; i++) {
+			builder.append(elements[i]);
+			if (i < elements.length - 1) {
+				builder.append(separator);
+			}
+		}
+
+		return builder.toString();
+	}
+
+	public void joinPrzemek(T... elements) {
+		for (int i = 0; i < elements.length; i++) {
+			if (i < elements.length - 1) {
+				System.out.print(elements[i] + separator);
+			} else {
+				System.out.print(elements[i]);
+			}
+		}
 	}
 
 }
--- src/main/java/com/sda/advanced/solution/zad27/Main.java
+++ src/main/java/com/sda/advanced/solution/zad27/Main.java
@@ -2,11 +2,20 @@ package com.sda.advanced.solution.zad27;
 
 import java.util.List;
 
+import com.sda.advanced.solution.zad11.Point2D;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Joiner<String> stringJoiner = new Joiner<>();
-		System.out.println(stringJoiner.join(". ", "a", "b", "c", "d")); // a. b. c. d
+		Joiner<String> stringJoiner = new Joiner<>(". ");
+
+		stringJoiner.joinPrzemek("a", "b", "c", "d"); // a. b. c. d
+		System.out.println();
+		System.out.println(stringJoiner.join("a", "b", "c", "d")); // a. b. c. d
+
+
+		Joiner<Point2D> point2DJoiner = new Joiner<>(System.lineSeparator());
+		System.out.println(point2DJoiner.join(new Point2D(0,0), new Point2D(1,1), new Point2D(5, -1)));
 	}
 
 }
